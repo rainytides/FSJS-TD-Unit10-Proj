ce717593d70cc1110e9450d3b37782059053ae66
Voke Elstein <141534071+rainytides@users.noreply.github.com>
2023-11-29 07:10:26 -0800

    enhanced comments

--- api/app.js
+++ api/app.js
@@ -1,39 +1,39 @@
 'use strict';
 
-// Load modules
+// Load required modules
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors'); // Import the cors module
+const cors = require('cors'); // CORS middleware for cross-origin requests
 
-// Import route modules
+// Importing user and course routes
 const userRoutes = require('./routes/users');
 const courseRoutes = require('./routes/courses');
 
 // Variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
-// Create the Express app
+// Create the Express application
 const app = express();
 
-// Enable CORS for requests from the React app
+// Enable CORS for the client app, allowing requests from React app at 'http://localhost:3000'
 app.use(cors({ origin: 'http://localhost:3000' }));
 
-// Setup request body JSON parsing
+// Middleware for parsing JSON request bodies
 app.use(express.json());
 
-// Setup morgan for HTTP request logging
+// Morgan middleware for logging HTTP requests
 app.use(morgan('dev'));
 
-// Setup the API routes
+// Setup API routes for users and courses
 app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
 
-// Setup a friendly greeting for the root route
+// Friendly greeting for the root route
 app.get('/', (req, res) => {
   res.send('Welcome to the REST API project!');
 });
 
-// Add a 404 handler for when a route is not found
+// Handler for non-existent routes
 app.use((req, res, next) => {
   const err = new Error('Route Not Found');
   err.status = 404;
@@ -52,12 +52,13 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Set the server port
+// Set server port
 app.set('port', process.env.PORT || 5000);
 
-// Start listening on the port
+// Start the server and listen on the specified port
 const server = app.listen(app.get('port'), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
 });
 
-module.exports = app; // Export the app for testing purposes
+// Export the Express app for testing purposes
+module.exports = app;
