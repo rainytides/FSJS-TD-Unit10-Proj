241e27c4e267e470eb908a065e3a4df396d08c36
Voke Elstein <141534071+rainytides@users.noreply.github.com>
2023-11-28 23:11:25 -0800

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+import Header from './components/Header';
+import UserSignUp from './components/user/UserSignUp';
+import UserSignIn from './components/user/UserSignIn';
+import UserSignOut from './components/user/UserSignOut';
+import Courses from './components/course/Courses';
+import CourseDetail from './components/course/CourseDetail';
+import CreateCourse from './components/course/CreateCourse';
+import UpdateCourse from './components/course/UpdateCourse';
+import PrivateRoute from './PrivateRoute';
+import withContext from './Context';
+import Forbidden from './components/Forbidden';
+import NotFound from './components/NotFound';
+import UnhandledError from './components/UnhandledError';
+
+// Higher-order components wrapping the original ones with additional context.
+const HeaderWithContext = withContext(Header);
+const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignInWithContext = withContext(UserSignIn);
+const UserSignOutWithContext = withContext(UserSignOut);
+const CoursesWithContext = withContext(Courses);
+const CourseDetailWithContext = withContext(CourseDetail);
+const CreateCourseWithContext = withContext(CreateCourse);
+const UpdateCourseWithContext = withContext(UpdateCourse);
+
+/**
+ * App - The main application component setting up the router and routes.
+ *
+ * This component uses React Router to define the navigation and routing
+ * for the application. It includes routes for signing up, signing in, 
+ * course creation and editing, and error handling.
+ */
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <HeaderWithContext />
+        <Switch>
+          <Route exact path='/' component={CoursesWithContext}/>
+          <PrivateRoute exact path='/courses/create' component={CreateCourseWithContext}/>
+          <Route exact path='/courses/:id' component={CourseDetailWithContext}/>
+          <PrivateRoute exact path='/courses/:id/update' component={UpdateCourseWithContext}/>
+
+          <Route path='/signup' component={UserSignUpWithContext} />
+          <Route path='/signin' component={UserSignInWithContext} />
+          <Route path='/signout' component={UserSignOutWithContext} />
+
+          <Route path='/forbidden' component={Forbidden} />
+          <Route path='/error' component={UnhandledError} />
+          <Route path='/notfound' component={NotFound} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
