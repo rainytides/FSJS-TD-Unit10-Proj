fd8bb663611f6872e52d7d5ab5e79da9e62cafc8
Voke Elstein <141534071+rainytides@users.noreply.github.com>
2023-12-01 17:30:28 -0800

    changes

--- client/src/components/Context/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const authUserContext = React.createContext();
-
-export const AuthProvider = authUserContext.Provider;
-export const AuthConsumer = authUserContext.Consumer;
-
-//Set up the Context API Provider & Consumer to be used for making authenticated user information available throughout the app
